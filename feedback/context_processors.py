051a83493f46f76d6af120627b85f30e5d4c8b8d
Matt <matt.leach25@gmail.com>
2015-03-28 18:45:44 +0000

    Making it look bootstrappy

--- feedback/context_processors.py
+++ feedback/context_processors.py
@@ -2,7 +2,7 @@ from feedback.forms import FeedbackForm
 
 
 def feedback(request):    
-    return {"feedback_form": FeedbackForm(initial={'url': request.build_absolute_uri()})}
+    return {"feedback_form": FeedbackForm(auto_id=False, initial={'url': request.build_absolute_uri()})}
 
     
     
--- feedback/forms.py
+++ feedback/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.forms.widgets import HiddenInput
 from feedback.models import Feedback
 
 
@@ -6,6 +7,12 @@ class FeedbackForm(ModelForm):
     class Meta:
         model = Feedback
         fields = ["text", "url"] # user, time added separately 
+
+    def __init__(self, *args, **kwargs):
+        super(FeedbackForm, self).__init__(*args, **kwargs)
+        self.fields['url'].widget = HiddenInput()
+        self.fields['text'].widget.attrs.update({"placeholder": "Enter feedback here"})
+        self.fields['text'].label = False
 
     class Media:
          css = {
