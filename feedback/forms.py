277ea03123157eb51262c77ed26e0fdff4b6940d
Matt <matt.leach25@gmail.com>
2015-03-28 17:46:53 +0000

    Adding FeedbackForm

---
+++ feedback/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from feedback.models import Feedback
+
+
+class FeedbackForm(ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ["text", "url"] # user, time added separately 
